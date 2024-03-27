f734aa602e036da66675be87b3c70c05e0f69b05
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-03-27 09:04:26 +0700

    cc

---
+++ tuan6/homepage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../tuan6/homepage.css">
+</head>
+<body>
+<div class='form'>
+        <div class='header'><img src="../tuan6/chuyendoiso.jpg" alt=""></div>
+        <div class='menu'>
+            <div class='f-menu'>Trang Chủ</div>
+            <div class='f-menu'>Diễn Đàn</div>
+            <div class='f-menu'>Tin Tức</div>
+            <div class='f-menu'>Hỏi Đáp</div>
+            <div class="dropdown">
+                    <button class="dropbtn">Danh mục
+                        <div class="dropdown-content">
+                            <a href="https://shopbongro.vn/">Chuyên đề tin học</a>
+                            <a href="https://iprice.vn/the-thao-dong-doi/bong-da/">Chuyên đề Âm Nhạc</a>
+                            <a href="https://shopbongchuyen.com/">Chuyên đề Thể Thao</a>
+                        </div>
+                    </button>
+            </div>
+        </div>
+        <!-- end menu -->
+        <div class='main'>
+            <div class='left'></div>
+            <div class='right'>
+            
+
+
+
+</div>
+    
+<!-- end HCN -->
+
+
+
+        </div>
+        <!-- end main -->
+
+        <div class='footer'>
+            <div class='khung'>
+            <div><b>CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BỒI DƯỠNG VĂN HÓA</b></div>
+            <div><i>Địa chỉ: 11 Đường 14B, Phường Bình Hưng Hòa A, Quận Bình Tân, tp.HCM</i></div>
+            <div><i>Điện thoại: 0903.97.92.18</i></div>
+            <div><i>Website: https://khonggiankythuat.com</i></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ tuan6/sql.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../tuan6/hp.css">
+</head>
+<body>
+    <?php
+    $conn = mysqli_connect("localhost","root","");
+    mysqli_select_db($conn,"qlysv");
+    mysqli_set_charset($conn,"utf8");
+    $sql = "select * from sinhvien order by masv";
+    $kq = mysqli_query($conn, $sql);
+    ?>
+    <?php 
+    if(mysqli_num_rows($kq)<>0)
+    {
+        echo"<table width='500' height ='25' border='0'>
+        <tr>
+        <td align='center'><h2><b>Danh sách sinh viên</b></h2></td>
+        </tr>
+        </table>
+        <table width='500' height='79' border='1'>
+        <tr>
+        <td width='44' height='23' align='center'>STT</td>
+        <td width='112' align='center'>Mã sính viên</td>
+        <td width='238' align='center'>Tên sinh viên</td>
+        <td width='78' align='center'>Phái</td>
+        </tr>";
+        $stt=1;
+        while($row=mysqli_fetch_row($kq))
+        {
+            echo"
+            <tr>
+            <td height='23' align='center'>".$stt."</td>
+            <td align='center'>".$row[0]."</td>";
+            echo"<td align='center'>".$row[1]."</td>"; 
+            
+            $stt++;
+        }
+
+        echo"</table>
+        ";
+
+    }
+    ?>
+</body>
+</html>
